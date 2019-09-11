8245ea7e5786dac3ab5447bd7f62a8160667150b
“Michael_McGuire“ <49034356+mwmcguire@users.noreply.github.com>
2019-09-10 23:22:13 -0400

    final components created, having issues with components rendering

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Jumbotron from '../components/Jumbotron';
+
+function NoMatch() {
+  return (
+    <div className="container">
+      <Jumbotron
+        image="http://www.justscience.in/wp-content/uploads/2018/01/what-would-happen-if-someone-detonated-a-nuclear-bomb-today.jpg"
+        title="404"
+        lead="Page Not Found"
+        instructions="Run for your Lives!!!"
+      />
+    </div>
+  )
+}
+
+export default NoMatch;
