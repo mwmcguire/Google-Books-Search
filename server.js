bb087935cddf91b33b5cf5e3560a7c988c2ca439
“Michael_McGuire“ <49034356+mwmcguire@users.noreply.github.com>
2019-09-16 20:13:49 -0400

    server changes for heroku

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+}
+
 // Define API routes
 app.use(routes);
 
@@ -31,16 +37,6 @@ db.once('open', _ => {
 db.on('error', err => {
   console.error('connection error:', err);
 });
-
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`API Server listening on port: ${PORT}`);
